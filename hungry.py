210533f18831c878d0734c4048fae1672f23a129
Munna <mehra.mkv@gmail.com>
2020-10-02 09:06:00 +0530

    Revert master

--- hungry.py
+++ hungry.py
@@ -3,10 +3,4 @@ if(hungry == "yes"):
     print("Eat Something:")
     print("Eat Pizza")
 else:
-    thirsty = input("Are You Thirsty?")
-    if(thirsty == "yes"):
-        print("Drink Water:")
-        print("Drink Soda:")
-    else:
-        print("Do Your HomeWork")
-
+    print("Do Your Homework")
